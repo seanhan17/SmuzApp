20c3bb66f72102678a25b434f20b51c1505ff942
seanhan17 <shenghan920@gmail.com>
2021-10-10 17:22:10 +0800

    Configured Breadcrumb

---
+++ SmuzApp.Web/Constant/BreadcrumbConstant.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmuzApp.Web.Constant
+{
+    public class BreadcrumbConstant
+    {
+        public const string BREADCRUMB_PARTIAL_VIEW_PATH = "../Shared/LayoutPanels/_Breadcrumb.cshtml";
+    }
+}
---
+++ SmuzApp.Web/ViewModels/BreadcrumbViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmuzApp.Web.ViewModels
+{
+    public class BreadcrumbViewModel
+    {
+        public string MainPageTitle { get; set; }
+        public List<Breadcrumb> BreadcrumbList { get; set; }
+    }
+
+    public class Breadcrumb
+    {
+        public string PageTitle { get; set; }
+        public string PageUrl { get; set; }
+        public bool isSelected { get; set; }
+    }
+}
